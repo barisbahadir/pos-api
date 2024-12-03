21c41b6d34801deece7a257ec792eacea9785dac
BARIS BAHADIR SAVUR <barisbahadir@live.com>
2024-12-03 23:00:27 +0300

    Api initializer add

---
+++ src/main/java/com/bahadir/pos/utils/ApiInitializer.java
@@ -0,0 +1,28 @@
+package com.bahadir.pos.utils;
+
+import com.bahadir.pos.entity.Category;
+import com.bahadir.pos.repository.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiInitializer implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+
+    public ApiInitializer(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Kategoriler tablosunda kayıt var mı kontrol et
+        if (categoryRepository.count() == 0) {
+            // Kayıt yoksa, yeni bir kategori ekle
+            Category category = new Category();
+            category.setName("Varsayilan Kategori");
+            categoryRepository.save(category);
+            System.out.println("Default category created!");
+        }
+    }
+}
