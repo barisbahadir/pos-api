4e3d178bdb11edf564302f36e1dd9d9613b391c9
BARIS BAHADIR SAVUR <barisbahadir@live.com>
2024-12-06 23:34:08 +0300

    Default user add

--- src/main/java/com/bahadir/pos/utils/ApiInitializer.java
+++ src/main/java/com/bahadir/pos/utils/ApiInitializer.java
@@ -1,17 +1,25 @@
 package com.bahadir.pos.utils;
 
 import com.bahadir.pos.entity.Category;
+import com.bahadir.pos.entity.User;
+import com.bahadir.pos.entity.UserRole;
 import com.bahadir.pos.repository.CategoryRepository;
+import com.bahadir.pos.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApiInitializer implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public ApiInitializer(CategoryRepository categoryRepository) {
+    public ApiInitializer(CategoryRepository categoryRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.categoryRepository = categoryRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -23,6 +31,17 @@ public class ApiInitializer implements CommandLineRunner {
             category.setName("POS");
             categoryRepository.save(category);
             System.out.println("Default category: 'POS' created!");
+        }
+
+        // Kullanicilar tablosunda kayıt var mı kontrol et
+        if (userRepository.count() == 0) {
+            // Kayıt yoksa, yeni bir kullanici ekle
+            User user = new User();
+            user.setEmail("bahadir");
+            user.setPassword(passwordEncoder.encode("bahadir"));
+            user.setRole(UserRole.USER);
+            userRepository.save(user);
+            System.out.println("Default user: 'BAHADIR' created!");
         }
     }
 }
