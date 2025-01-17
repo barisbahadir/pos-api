fd0d84f20d03d8bff6ccbe973864d12594629fb0
BARIS BAHADIR SAVUR <barisbahadir@live.com>
2025-01-17 09:36:58 +0300

    Daily sales report date format add

--- src/main/java/com/bahadir/pos/entity/report/DailySalesReport.java
+++ src/main/java/com/bahadir/pos/entity/report/DailySalesReport.java
@@ -1,5 +1,6 @@
 package com.bahadir.pos.entity.report;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
@@ -7,6 +8,8 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import static com.bahadir.pos.utils.DateTimeUtils.DEFAULT_DATE_FORMAT;
 
 @Data
 @Getter
@@ -18,7 +21,9 @@ public class DailySalesReport {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DEFAULT_DATE_FORMAT)
     private LocalDate reportDate;
+
     private BigDecimal cardSales;
     private BigDecimal cashSales;
     private BigDecimal totalAmount;
