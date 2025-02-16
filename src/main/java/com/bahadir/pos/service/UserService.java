f7f1be7524be7f100a466e2ecbd4ba6b07b6eab5
BARIS BAHADIR SAVUR <barisbahadir@live.com>
2025-02-16 11:52:55 +0300

    User role name change

--- src/main/java/com/bahadir/pos/service/UserService.java
+++ src/main/java/com/bahadir/pos/service/UserService.java
@@ -1,6 +1,6 @@
 package com.bahadir.pos.service;
 
-import com.bahadir.pos.entity.user.AuthRole;
+import com.bahadir.pos.entity.user.UserRole;
 import com.bahadir.pos.entity.user.User;
 import com.bahadir.pos.exception.ApiException;
 import com.bahadir.pos.repository.UserRepository;
@@ -35,7 +35,7 @@ public class UserService {
         User user = User.builder()
                 .email(email)
                 .password(encodedPassword)
-                .authRole(AuthRole.USER)
+                .userRole(UserRole.USER)
                 .build();
 
         return userRepository.save(user);
