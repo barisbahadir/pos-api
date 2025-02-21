ebb157ff33ceb59a797575b78fe300bc70ad56ad
BARIS BAHADIR SAVUR <barisbahadir@live.com>
2025-02-21 15:48:58 +0300

    Exception handle update

--- src/main/java/com/bahadir/pos/controller/CustomErrorController.java
+++ src/main/java/com/bahadir/pos/controller/CustomErrorController.java
@@ -1,14 +1,15 @@
 package com.bahadir.pos.controller;
 
+import com.bahadir.pos.exception.JwtTokenException;
 import com.bahadir.pos.utils.ApiResponse;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.text.MessageFormat;
 
 @RestController
 @RequestMapping("/error")
@@ -18,11 +19,17 @@ public class CustomErrorController {
     public ResponseEntity<ApiResponse<?>> handleError(HttpServletRequest request) {
         Throwable exception = (Throwable) request.getAttribute("jakarta.servlet.error.exception");
         int statusCode = (int) request.getAttribute("jakarta.servlet.error.status_code");
-        HttpStatus defaultHttpStatus = HttpStatus.FORBIDDEN;
+        HttpStatus httpStatus;
 
-        ApiResponse<Void> response = ApiResponse.error(defaultHttpStatus.value(), "(GLOBAL): " + MessageFormat.format("{0}", exception != null ? exception.getMessage() : "Unknown error"));
+        if (exception instanceof JwtException || exception instanceof JwtTokenException || exception instanceof AccessDeniedException) {
+            httpStatus = HttpStatus.UNAUTHORIZED;  // Örneğin, geçersiz argüman hatası
+        } else {
+            httpStatus = HttpStatus.FORBIDDEN;
+        }
 
-        return new ResponseEntity<>(response, defaultHttpStatus);
+        ApiResponse<Void> response = ApiResponse.error(httpStatus.value(), exception != null ? exception.getMessage() : "(GLOBAL): Unknown error");
+
+        return new ResponseEntity<>(response, httpStatus);
     }
 }
 
